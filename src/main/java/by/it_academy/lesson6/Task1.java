30823668d32b6a8f618f3d5874590c6690c06b95
ivanpc313 <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>
2022-10-04 10:43:55 +0300

    lesson6 (corrected)

--- src/main/java/by/it_academy/lesson6/Task1.java
+++ src/main/java/by/it_academy/lesson6/Task1.java
@@ -6,9 +6,8 @@ public class Task1 {
 
         String someText = "I love Java!";
         StringBuilder stringBuilder = new StringBuilder();
-        int stringLength = someText.length();
 
-        for (int i = stringLength - 1; i >= 0; i--) {
+        for (int i = someText.length() - 1; i >= 0; i--) {
 
             stringBuilder.append(someText.charAt(i));
         }
