d821d025990512820f58e07fc945af4063aff70d
Maxim Tereshchenko <maksimt@backbase.com>
2022-10-31 16:57:42 +0100

    fixed

--- src/main/java/by/it_academy/lesson13/task1/Main.java
+++ src/main/java/by/it_academy/lesson13/task1/Main.java
@@ -1,6 +1,5 @@
 package by.it_academy.lesson13.task1;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
@@ -9,48 +8,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Thread positiveEvenThread = new Thread(Main::positiveEvenPrint);
+        List<Integer> numbers = randomNumbers();
+        System.out.println(numbers);
+
+        Thread positiveEvenThread = new Thread(() -> positiveEvenPrint(numbers));
         positiveEvenThread.start();
         positiveEvenThread.setName("Even numbers: ");
 
-        Thread negativeThread = new Thread(Main::negativePrint);
+        Thread negativeThread = new Thread(() -> negativePrint(numbers));
         negativeThread.start();
         negativeThread.setName("Negative numbers: ");
 
-        Thread positiveOddThread = new Thread(Main::positiveOddPrint);
+        Thread positiveOddThread = new Thread(() -> positiveOddPrint(numbers));
         positiveOddThread.start();
         positiveOddThread.setName("Positive odd numbers: ");
     }
 
     private static List<Integer> randomNumbers() {
         return ThreadLocalRandom.current()
-                .ints(10)
-                .boxed()
-                .collect(Collectors.toList());
+            .ints(10)
+            .boxed()
+            .collect(Collectors.toList());
 
     }
 
-    private static void positiveEvenPrint() {
-        Collection<Integer> numbers = randomNumbers();
+    private static void positiveEvenPrint(List<Integer> numbers) {
         List<Integer> positiveEvenNumber = numbers.stream()
-                .filter(number -> number > 0 && number % 2 == 0)
-                .collect(Collectors.toList());
+            .filter(number -> number > 0 && number % 2 == 0)
+            .collect(Collectors.toList());
         System.out.println(Thread.currentThread().getName() + " " + positiveEvenNumber);
     }
 
-    private static void negativePrint() {
-        Collection<Integer> numbers = randomNumbers();
+    private static void negativePrint(final List<Integer> numbers) {
         List<Integer> negativeNumber = numbers.stream()
-                .filter(number -> number < 0)
-                .collect(Collectors.toList());
+            .filter(number -> number < 0)
+            .collect(Collectors.toList());
         System.out.println(Thread.currentThread().getName() + " " + negativeNumber);
     }
 
-    private static void positiveOddPrint() {
-        Collection<Integer> numbers = randomNumbers();
+    private static void positiveOddPrint(final List<Integer> numbers) {
         List<Integer> positiveOddNumber = numbers.stream()
-                .filter(number -> number > 0 && number % 2 != 0)
-                .collect(Collectors.toList());
+            .filter(number -> number > 0 && number % 2 != 0)
+            .collect(Collectors.toList());
         System.out.println(Thread.currentThread().getName() + " " + positiveOddNumber);
     }
+
 }
--- src/main/java/by/it_academy/lesson13/task2/Main.java
+++ src/main/java/by/it_academy/lesson13/task2/Main.java
@@ -3,32 +3,30 @@ package by.it_academy.lesson13.task2;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        states();
-    }
-
-
-    private static void states() throws InterruptedException {
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-
+        Thread endless = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
             }
-        };
+        });
+        endless.start();
 
-        System.out.println(thread.getState());
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                endless.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        System.out.println(thread.getState()); // New
         thread.start();
-        System.out.println(thread.getState());
+        System.out.println(thread.getState()); // Runnable
+        Thread.sleep(500);
+        System.out.println(thread.getState()); // probably Timed waiting
         Thread.sleep(1000);
-        System.out.println(thread.getState());
+        System.out.println(thread.getState()); // probably Waiting
+        endless.interrupt();
         thread.join();
-        System.out.println(thread.getState());
+        System.out.println(thread.getState()); // Terminated
     }
-
-
 }
